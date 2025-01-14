d74c6f51b1cff67ba5f4f2ad720d10cc9a17c3fe
jemmy1794 <jemmy17941997@gmail.com>
2025-01-14 12:50:33 +0800

    Update 1_two_sum.cpp

--- 1_two_sum.cpp
+++ 1_two_sum.cpp
@@ -1,31 +1,18 @@
-#include <iostream>
-#include <vector>
-
 class Solution {
 public:
-    std::vector<int> twoSum(std::vector<int>& nums, int target) {
-        // std::cout << nums.size() << std::endl;
-        std::vector<int> result;
-        for(int i=0;i<nums.size();i++){
-            for(int j=i+1;j<nums.size();j++){
-                if((nums[i]+nums[j]) == target){
-                    result.push_back(i);
-                    result.push_back(j);
-                }
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        std::unordered_map<int, int> umap;
+        for(int i = 0; i < nums.size(); i++){
+            int tmp = target - nums[i];
+            if(umap.contains(tmp)){
+                ans.push_back(umap[tmp]);
+                ans.push_back(i);
+                break;
+            }else{
+                umap[nums[i]] = i;
             }
         }
-        return result;
-    }
-    Solution(){
-        std::cout << "class Solution constructed" << std::endl;
+        return ans;
     }
 };
-
-int main(){
-    Solution solution;
-    std::vector<int> nums = {3, 2, 4};
-    int target = 6;
-    std::vector<int> result = solution.twoSum(nums, target);
-    std::cout << "Indices: " << result[0] << ", " << result[1] << std::endl;
-    return 0;
-}
